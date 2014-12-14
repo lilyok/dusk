0d88df9acc73d727f4a5c4516b534f2382b80137
lilyok <lilyoknabieva@gmail.com>
2014-12-15 00:43:00 +0300

    initial

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,123 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = LabelTTF::create("Hello World", "Arial", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+
+
+    //////////////////////////////////////////////////
+    // load the Sprite Sheet
+    auto spritecache = SpriteFrameCache::getInstance();
+    
+    // the .plist file can be generated with any of the tools mentioned below
+    spritecache->addSpriteFramesWithFile("tgirl.plist");
+    
+    auto mysprite = Sprite::createWithSpriteFrameName("Thumbelina01.png");
+    // position the sprite on the center of the screen
+    mysprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    
+    // now lets animate the sprite we moved
+    
+    Vector<SpriteFrame*> animFrames;
+    animFrames.reserve(12);
+    animFrames.pushBack(spritecache->getSpriteFrameByName("Thumbelina01.png"));
+    animFrames.pushBack(spritecache->getSpriteFrameByName("Thumbelina02.png"));
+    animFrames.pushBack(spritecache->getSpriteFrameByName("Thumbelina03.png"));
+    animFrames.pushBack(spritecache->getSpriteFrameByName("Thumbelina04.png"));
+
+    
+    // create the animation out of the frames
+    Animation* animation = Animation::createWithSpriteFrames(animFrames, 0.1f);
+    Animate* animate = Animate::create(animation);
+    
+    // run it and repeat it forever
+    mysprite->runAction(RepeatForever::create(animate));
+    
+    // add the sprite as a child to this layer
+    this->addChild(mysprite, 0);
+    ///////////////////////////////////////////
+//    // add "HelloWorld" splash screen"
+//    auto sprite = Sprite::create("HelloWorld.png");
+//    
+//    // position the sprite on the center of the screen
+//    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+//
+//    // add the sprite as a child to this layer
+//    this->addChild(sprite, 0);
+    
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
