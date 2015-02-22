42eea81e2069b8604fd24c6882e51ac850b02a70
lilyok <lilyoknabieva@gmail.com>
2015-02-23 02:09:44 +0300

    added thirdscene class

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../assets/TileMaps)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -12,9 +13,15 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/MenuScene.cpp \
+                   ../../Classes/FirstScene.cpp \
+                   ../../Classes/SecondScene.cpp \
+                   ../../Classes/ThirdScene.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes  \
+                    $(LOCAL_PATH)/../assets/TileMaps
+                    
+
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
